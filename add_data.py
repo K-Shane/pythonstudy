42ed2d5c7fb0f52ed7b1acd0fef439d0d033e747
Seungwoo Koh <127972359+K-Shane@users.noreply.github.com>
2023-08-15 10:20:04 +0900

    Delete add_data.py

--- add_data.py
+++
@@ -1,7 +0,0 @@
-# add_data.py
-# 쓰기 모드로 열고 11~19번째줄 추가
-f = open("C:/doit/새파일.txt",'a',encoding="utf-8")
-for i in range(11, 20):
-    data = "%d번째 줄입니다%s\n" % (i,i*"!")
-    f.write(data)
-f.close()
