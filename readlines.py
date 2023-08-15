eef61daac4e534404c4e591dfe8874443ffcddb3
Seungwoo Koh <127972359+K-Shane@users.noreply.github.com>
2023-08-15 10:20:52 +0900

    Delete readlines.py

--- readlines.py
+++
@@ -1,9 +0,0 @@
-# readlines.py
-#readlines 함수는 파일의 모든 줄을 읽어서 각각의 줄을 요소로 가지는 리스트를 리턴
-#strip()함수를 활용하여 맨 뒷쪽 줄바꿈 표시 혹은 공백들을 제거함
-f = open("C:/doit/새파일.txt", 'r', encoding="utf-8")
-lines = f.readlines()
-for line in lines:
-    line=line.strip()
-    print(line)
-f.close()
